201c68b2b553b55d919f2a901bfb1c08322d4252
Thomas <31560900+0xtlt@users.noreply.github.com>
2026-04-01 14:04:02 +0200

    fix: update SHA256 for v1.0.0

--- Casks/tiler.rb
+++ Casks/tiler.rb
@@ -3,12 +3,12 @@ cask "tiler" do
 
   on_arm do
     url "https://github.com/0xtlt/tiler/releases/download/v#{version}/Tiler-arm64.dmg"
-    sha256 "de90039a7e38221e62cb5b68ac067d7c76133890613d09254bdcdb890847c19c"
+    sha256 "aa67135dd264b1ecc99a4e10902dfc6c87d34081d4fa6cfeffdec7490eb8c2e6"
   end
 
   on_intel do
     url "https://github.com/0xtlt/tiler/releases/download/v#{version}/Tiler-x86_64.dmg"
-    sha256 "ee41271c6ffaf0047d07bfd1c52668b063cb866358f30df4b6323b924b9b1d2c"
+    sha256 "bfa1409352d45505426d9e792337283dd3b424800c8542fd1084454e34e56408"
   end
 
   name "Tiler"
