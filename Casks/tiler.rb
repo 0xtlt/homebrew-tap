36de60225f34733b73ce914d99fad5242bd897e4
Thomas <31560900+0xtlt@users.noreply.github.com>
2026-04-03 14:19:38 +0200

    chore: bump tiler to 1.1.0

--- Casks/tiler.rb
+++ Casks/tiler.rb
@@ -1,14 +1,14 @@
 cask "tiler" do
-  version "1.0.0"
+  version "1.1.0"
 
   on_arm do
     url "https://github.com/0xtlt/tiler/releases/download/v#{version}/Tiler-arm64.dmg"
-    sha256 "aa67135dd264b1ecc99a4e10902dfc6c87d34081d4fa6cfeffdec7490eb8c2e6"
+    sha256 "b050b12d86c1c8f9b67cc2462e7434c54cd51423d15be7035ec8b014765a8369"
   end
 
   on_intel do
     url "https://github.com/0xtlt/tiler/releases/download/v#{version}/Tiler-x86_64.dmg"
-    sha256 "bfa1409352d45505426d9e792337283dd3b424800c8542fd1084454e34e56408"
+    sha256 "fd292b121a56985da58aa1f88f9510660f275d21479f9958dd6017f998e9831a"
   end
 
   name "Tiler"
