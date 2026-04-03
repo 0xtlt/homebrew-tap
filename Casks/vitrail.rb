eb9bdfb174dd7864d3d4cf4d779acb0adef3814e
Thomas <31560900+0xtlt@users.noreply.github.com>
2026-04-03 19:10:25 +0200

    chore: bump vitrail to 2.1.0

--- Casks/vitrail.rb
+++ Casks/vitrail.rb
@@ -1,14 +1,14 @@
 cask "vitrail" do
-  version "2.0.0"
+  version "2.1.0"
 
   on_arm do
     url "https://github.com/0xtlt/vitrail/releases/download/v#{version}/Vitrail-arm64.dmg"
-    sha256 "6c8ace9ca39b7831c2a6f54433ebbcb5daf8b347812b1f6f327bf54042ac9d11"
+    sha256 "a39afcf3c66f9dc18a442dd011a50ea61efe5afd2344d71bd2e43db9fa24e599"
   end
 
   on_intel do
     url "https://github.com/0xtlt/vitrail/releases/download/v#{version}/Vitrail-x86_64.dmg"
-    sha256 "1992025fd7cd36edc418ce700e1db7f936c9c04b8018e5da6165e23f59745431"
+    sha256 "c5616d73a6136a7e7814d77aec404205e051686d5b11343fd11605c604b3a366"
   end
 
   name "Vitrail"
