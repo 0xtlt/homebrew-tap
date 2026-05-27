da71596e4719f222510b61611822db1e55504c9a
Thomas <31560900+0xtlt@users.noreply.github.com>
2026-05-27 17:02:29 +0200

    chore: update mactmux cask to 0.1.0-beta.6

--- Casks/mactmux.rb
+++ Casks/mactmux.rb
@@ -1,9 +1,9 @@
 cask "mactmux" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.1.0-beta.5"
-  sha256 arm:   "c1160288c2db5088079bc06ee01488ea5962d70b824f9558534434b54eecab87",
-         intel: "29b7be31177fb59161638edb1e615ab9de0cd1bb94b157abdd3c53a689ea5818"
+  version "0.1.0-beta.6"
+  sha256 arm:   "b5a3daf065314ed207e75644e622c3c0980d4bfe406ad38bc7b7c22fb0445c29",
+         intel: "504da1947a4633091f273a7e3b0b915d1e3fb2260f161c6b7379881c845f340e"
 
   url "https://github.com/0xtlt/MacTMUX/releases/download/v#{version}/mactmux-#{arch}.dmg"
   name "MacTMUX"
